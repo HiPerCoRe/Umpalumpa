ee79da978a7f8a97d2d8fab6e8e5e8f027594e6a
David Strelak <davidstrelak@gmail.com>
2021-02-12 11:08:55 +0100

    cmake tutorial

---
+++ build_example_cmake/helloworld.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "TutorialConfig.h"
+
+
+int main(int argc, char**argv){
+	std::cout << "Hello world!" << std::endl;
+	// report version
+    std::cout << argv[0] << " Version " << Tutorial_VERSION_MAJOR << "."
+              << Tutorial_VERSION_MINOR << std::endl;
+    std::cout << "Usage: " << argv[0] << " number" << std::endl;
+    return 1;
+}
