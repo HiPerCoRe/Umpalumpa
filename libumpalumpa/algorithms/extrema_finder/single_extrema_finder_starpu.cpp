636b5564e2add4f2ff4dcd6abd1bd3c67e9a825e
David Strelak <davidstrelak@gmail.com>
2021-04-07 13:59:06 +0200

    using conditional handler

--- libumpalumpa/algorithms/extrema_finder/single_extrema_finder_starpu.cpp
+++ libumpalumpa/algorithms/extrema_finder/single_extrema_finder_starpu.cpp
@@ -38,25 +38,27 @@ namespace extrema_finder {
       starpu_data_set_name(hVal, out.values->description.c_str());
     }
 
-    // starpu_data_handle_t hLoc = { 0 }; // FIXME deal with optional handles
-    // if (nullptr != out.locations) {
-    //   starpu_payload_register(&hLoc, STARPU_MAIN_RAM, *out.locations);
-    //   starpu_data_set_name(hLoc, out.locations->description.c_str());
-    // }
+    starpu_data_handle_t hLoc = { 0 };
+    if (nullptr != out.locations) {
+      starpu_payload_register(&hLoc, STARPU_MAIN_RAM, *out.locations);
+      starpu_data_set_name(hLoc, out.locations->description.c_str());
+    } else {
+      starpu_void_data_register(&hLoc);
+    }
 
     struct starpu_task *task = starpu_task_create();
     task->handles[0] = hIn;
     task->handles[1] = hVal;
-    // task->handles[2] = hLoc;
+    task->handles[2] = hLoc;
     task->cl_arg = new Settings(settings);
     task->cl_arg_size = sizeof(Settings);
     task->cl = [] {
       static starpu_codelet c;
       c.where = STARPU_CPU;
-      c.cpu_funcs[0] = cpu, c.nbuffers = 2;
+      c.cpu_funcs[0] = cpu, c.nbuffers = 3;
       c.modes[0] = STARPU_R;
       c.modes[1] = STARPU_W;
-      // c.modes[2] = STARPU_W;
+      c.modes[2] = STARPU_W;
       return &c;
     }();
 
@@ -64,7 +66,7 @@ namespace extrema_finder {
     STARPU_CHECK_RETURN_VALUE(starpu_task_submit(task), "starpu_task_submit %s", this->taskName);
     starpu_data_unregister_submit(hIn);
     starpu_data_unregister_submit(hVal);
-    // starpu_data_unregister_submit(hLoc);
+    starpu_data_unregister_submit(hLoc);
     return true;
   }
 }// namespace extrema_finder
