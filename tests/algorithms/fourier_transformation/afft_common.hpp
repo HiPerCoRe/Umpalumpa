3485a3d2ab45c7c6d3b694e06a96d6c961bd2efb
David Myska <456346@mail.muni.cz>
2021-08-12 08:49:59 +0200

    Added test forward-inverse FFT

--- tests/algorithms/fourier_transformation/afft_common.hpp
+++ tests/algorithms/fourier_transformation/afft_common.hpp
@@ -130,3 +130,42 @@ TEST_F(NAME, InpulseShiftedForward)
     Free(out);
   }
 }
+
+TEST_F(NAME, FFTIFFT)
+{
+  Locality locality = Locality::kOutOfPlace;
+  Direction direction = Direction::kForward;
+  Settings settings(locality, direction);
+
+  Size size(5, 5, 1, 1);
+
+  PhysicalDescriptor pdIn(size.total * sizeof(float), DataType::kFloat);
+  FourierDescriptor ldIn(size, size);
+
+  void *in = Allocate(pdIn.bytes);
+  memset(in, 0, pdIn.bytes);
+  //CudaErrchk(cudaMemset(in, 0, pdIn.bytes))
+  //auto *in = calloc(pdIn.bytes, 1); // FIXME should be pre-allocated before the test, and should be reused in more 
+  auto inP = AFFT::InputData(Payload(in, ldIn, pdIn, "Input data"));
+
+  FourierDescriptor ldOut(size, size, FourierDescriptor::FourierSpaceDescriptor()); // copy, because they describe the same data
+  PhysicalDescriptor pdOut(ldOut.GetPaddedSize().total * 2 * sizeof(float), DataType::kFloat);
+
+  void *out;
+  if (settings.IsOutOfPlace()) {
+    out = Allocate(pdOut.bytes);
+    //CudaErrchk(cudaMemset(out, 0, pdIn.bytes))
+    //out = calloc(pdOut.bytes, 1);
+  } else {
+    out = in;
+  }
+
+  auto outP = AFFT::ResultData(Payload(out, ldOut, pdOut, "Result data"));
+
+  testFFTIFFT(outP, inP, settings);
+
+  Free(in);
+  if ((void*)in != (void*)out) {
+    Free(out);
+  }
+}
--- tests/algorithms/fourier_transformation/fft_tests.hpp
+++ tests/algorithms/fourier_transformation/fft_tests.hpp
@@ -1,3 +1,4 @@
+#include <fcntl.h>
 #include <gtest/gtest.h>
 #include <libumpalumpa/algorithms/fourier_transformation/settings.hpp>
 #include <libumpalumpa/algorithms/fourier_transformation/locality.hpp>
@@ -5,9 +6,25 @@
 #include <libumpalumpa/algorithms/fourier_transformation/afft.hpp>
 #include <libumpalumpa/algorithms/fourier_transformation/fft_cuda.hpp>
 #include <complex>
+#include <random>
 
 using namespace umpalumpa::fourier_transformation;
 using namespace umpalumpa::data;
+
+//FIXME make some utility file, for similar functions needed in tests
+template<typename T> void FillRandomBytes(T *dst, size_t bytes)
+{
+  int fd = open("/dev/urandom", O_RDONLY);
+  read(fd, dst, bytes);
+  close(fd);
+}
+
+template<typename T> void GenerateData(T *data, size_t elems)
+{
+  auto mt = std::mt19937(42);
+  auto dist = std::normal_distribution<float>((float)0, (float)1);
+  for (size_t i = 0; i < elems; ++i) { data[i] = dist(mt); }
+}
 
 class FFT_Tests 
 {
@@ -104,6 +121,49 @@ class FFT_Tests
         auto mag = re * re + im * im;
         ASSERT_NEAR(1.f, std::sqrt(mag), delta) << " at " << i;
       }
+    }
+
+    void testFFTIFFT(AFFT::ResultData &out, AFFT::InputData &in, const Settings &settings) {
+      auto &inverseIn = out;
+      auto &inverseOut = in;
+
+      auto *ref = new float[in.data.info.GetPaddedSize().total];
+      GenerateData(ref, in.data.info.GetPaddedSize().total);
+      memcpy(in.data.data, ref, in.data.info.GetPaddedSize().total * sizeof(float));
+
+      PrintData((float*)in.data.data, in.data.info.GetPaddedSize());
+
+      auto &ft = GetTransformer();
+
+      //FIXME we assume that settings is set as forward fft
+      ft.Init(out, in, settings);
+      ft.Execute(out, in);
+      ft.Synchronize();
+
+      PrintData((std::complex<float>*)out.data.data, out.data.info.GetPaddedSize());
+
+      ft.Init(inverseOut, inverseIn, settings.CreateInverse());
+      ft.Execute(inverseOut, inverseIn);
+      ft.Synchronize();
+
+      PrintData((float*)in.data.data, in.data.info.GetPaddedSize());
+
+      float delta = 0.00001f;
+      for (size_t n = 0; n < inverseOut.data.info.GetSize().n; ++n) {
+        size_t offset = n * inverseOut.data.info.GetPaddedSize().single;
+        // skip the padded area, it can contain garbage data
+        for (size_t z = 0; z < inverseOut.data.info.GetSize().z; ++z) {
+          for (size_t y = 0; y < inverseOut.data.info.GetSize().y; ++y) {
+            for (size_t x = 0; x < inverseOut.data.info.GetSize().x; ++x) {
+              size_t index = offset + z * inverseOut.data.info.GetSize().x * inverseOut.data.info.GetSize().y + y * inverseOut.data.info.GetSize().x + x;
+              auto *tmp = reinterpret_cast<float*>(inverseOut.data.data);
+              ASSERT_NEAR(ref[index], tmp[index] / inverseOut.data.info.GetSize().single, delta) << "at " << index;
+            }
+          }
+        }
+      }
+
+      delete[] ref;
     }
 
   protected:
