ea790faaa3e1d6495f06c24af3dd8b0f06541276
David Myska <456346@mail.muni.cz>
2021-08-13 13:05:31 +0200

    Extracted setup function into a common parent for all the FFT tests

--- tests/algorithms/fourier_transformation/fft_cuda.cpp
+++ tests/algorithms/fourier_transformation/fft_cuda.cpp
@@ -11,48 +11,21 @@ using namespace umpalumpa::data;
 class FFTCUDATest : public ::testing::Test, public FFT_Tests
 {
 public:
-  static auto Allocate(size_t bytes)
-  {
+  void *Allocate(size_t bytes) override {
     void *ptr;
     CudaErrchk(cudaMallocManaged(&ptr, bytes));
     return ptr;
   }
 
-  static void Free(void *ptr) { cudaFree(ptr); }
+  void Free(void *ptr) override { cudaFree(ptr); }
 
-  // Deliberately not using gtest's SetUp method, because we need to know Settings and
-  // Size of the current test
-  // ONLY float currently supported!!
-  auto SetUpFFT(const Settings &settings, const Size &size, const Size &paddedSize) {
-    ldSpatial = std::make_unique<FourierDescriptor>(size, paddedSize);
-    auto spatialSizeInBytes = ldSpatial->GetPaddedSize().total * Sizeof(DataType::kFloat);
-    pdSpatial = std::make_unique<PhysicalDescriptor>(spatialSizeInBytes, DataType::kFloat);
-
-    dataSpatial = std::shared_ptr<void>(Allocate(pdSpatial->bytes), FFTCUDATest::Free);
-    memset(dataSpatial.get(), 0, pdSpatial->bytes);
-
-    ldFrequency = std::make_unique<FourierDescriptor>(size, paddedSize, FourierDescriptor::FourierSpaceDescriptor());
-    auto frequencySizeInBytes = ldFrequency->GetPaddedSize().total * Sizeof(DataType::kFloat) * 2;
-    pdFrequency = std::make_unique<PhysicalDescriptor>(frequencySizeInBytes, DataType::kFloat);
-
-    if (settings.IsOutOfPlace()) {
-      dataFrequency = std::shared_ptr<void>(Allocate(pdFrequency->bytes), FFTCUDATest::Free);
-    } else {
-      dataFrequency = dataSpatial;
-    }
-  }
+  // CANNOT return "Free" method, because of the destruction order
+  FreeFunction GetFree() override { return [](void *ptr){ CudaErrchk(cudaFree(ptr));}; }
 
   FFTCUDA &GetTransformer() override { return transformer; }
 
 protected:
   FFTCUDA transformer = FFTCUDA(0);
-
-  std::shared_ptr<void> dataSpatial;
-  std::unique_ptr<PhysicalDescriptor> pdSpatial;
-  std::unique_ptr<FourierDescriptor> ldSpatial;
-  std::shared_ptr<void> dataFrequency;
-  std::unique_ptr<PhysicalDescriptor> pdFrequency;
-  std::unique_ptr<FourierDescriptor> ldFrequency;
 };
 #define NAME FFTCUDATest
 #include <tests/algorithms/fourier_transformation/afft_common.hpp>
