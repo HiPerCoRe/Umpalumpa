31ebd92ef7de17d6ff93ce235e540c314af4442f
David Myska <456346@mail.muni.cz>
2021-10-24 10:32:30 +0200

    Cleaned up KTTHelper

--- libumpalumpa/tuning/ktt_helper.hpp
+++ libumpalumpa/tuning/ktt_helper.hpp
@@ -1,13 +1,9 @@
 #pragma once
 
-#include <memory>
 #include <mutex>
-#include <map>
-#include <vector>
 #include <libumpalumpa/system_includes/ktt.hpp>
 #include <libumpalumpa/utils/cuda.hpp>
 #include <libumpalumpa/utils/system.hpp>
-
 
 namespace umpalumpa {
 namespace utils {
@@ -15,12 +11,6 @@ namespace utils {
   {
 
   public:
-    struct KernelIds
-    {
-      std::vector<ktt::KernelDefinitionId> definitionIds;
-      std::vector<ktt::KernelId> kernelIds;
-    };
-
     KTTHelper(const CUcontext context, const std::vector<ktt::ComputeQueue> &queues)
       : tuner(ktt::ComputeApi::CUDA, ktt::ComputeApiInitializer(context, queues))
     {
@@ -43,24 +33,12 @@ namespace utils {
       // be careful not to remove queue used by another algorithm
     }
 
-    auto &GetKernelData(std::string className)
-    {
-      auto it = kernelData.find(className);
-      if (kernelData.end() == it) {
-        // not found, insert it
-        kernelData[className] = {};
-        // assume that we don't have too many kernels -> find is cheap
-        it = kernelData.find(className);
-      }
-      return it->second;
-    }
-
-    // FIXME add method to access correct stream (and remove interface.GetAllQueues().at(0) from strategy calls)
+    // FIXME add method to access correct stream (and remove interface.GetAllQueues().at(0) from
+    // strategy calls)
 
   private:
     ktt::Tuner tuner;
     std::mutex mutex;
-    std::map<std::string, std::map<size_t, KernelIds>> kernelData;
   };
 
 }// namespace utils
