871653a56ff8b326c0e0eb6061f1f8f242d82838
David Myska <456346@mail.muni.cz>
2021-10-26 17:18:34 +0200

    Initialized tests for MultiPayloadWrapper

---
+++ tests/data/payload_wrapper/multi_payload_wrapper.cpp
@@ -0,0 +1,12 @@
+#include <gtest/gtest.h>
+
+class MultiPayloadWrapperTest : public ::testing::Test
+{
+  // TODO
+};
+
+TEST_F(MultiPayloadWrapperTest, Create)
+{
+  ASSERT_TRUE(true);
+  ASSERT_FALSE(false);
+}
