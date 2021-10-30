28ee2fb82aea61ff20b508602750f39d2eca856e
David Strelak <davidstrelak@gmail.com>
2021-10-30 21:02:33 +0200

    adding StarPU version of the Fourier processing

--- libumpalumpa/algorithms/fourier_processing/fp_starpu.cpp
+++ libumpalumpa/algorithms/fourier_processing/fp_starpu.cpp
@@ -1,23 +1,23 @@
-#include <libumpalumpa/algorithms/correlation/correlation_starpu.hpp>
-#include <libumpalumpa/algorithms/correlation/correlation_cpu.hpp>
-#include <libumpalumpa/algorithms/correlation/correlation_cuda.hpp>
+#include <libumpalumpa/algorithms/fourier_processing/fp_starpu.hpp>
+#include <libumpalumpa/algorithms/fourier_processing/fp_cpu.hpp>
+#include <libumpalumpa/algorithms/fourier_processing/fp_cuda.hpp>
 #include <libumpalumpa/system_includes/spdlog.hpp>
 
-namespace umpalumpa::correlation {
+namespace umpalumpa::fourier_processing {
 namespace {// to avoid poluting
   struct ExecuteArgs
   {
     Settings settings;
-    const std::vector<std::unique_ptr<ACorrelation>> *algs;
+    const std::vector<std::unique_ptr<AFP>> *algs;
   };
 
   void Codelet(void *buffers[], void *func_arg)
   {
-    auto *outP = reinterpret_cast<ACorrelation::OutputData::PayloadType *>(buffers[0]);
-    auto out = ACorrelation::OutputData(std::move(*outP));
-    auto *inP1 = reinterpret_cast<ACorrelation::InputData::PayloadType *>(buffers[1]);
-    auto *inP2 = reinterpret_cast<ACorrelation::InputData::PayloadType *>(buffers[2]);
-    auto in = ACorrelation::InputData(std::move(*inP1), std::move(*inP2));
+    auto *outP = reinterpret_cast<AFP::OutputData::DataType *>(buffers[0]);
+    auto out = AFP::OutputData(std::move(*outP));
+    auto *inD = reinterpret_cast<AFP::InputData::DataType *>(buffers[1]);
+    auto *inF = reinterpret_cast<AFP::InputData::FilterType *>(buffers[2]);
+    auto in = AFP::InputData(std::move(*inD), std::move(*inF));
     auto *args = reinterpret_cast<ExecuteArgs *>(func_arg);
     auto &alg = args->algs->at(static_cast<size_t>(starpu_worker_get_id()));
     std::ignore = alg->Execute(out, in);// we have no way of comunicate the result
@@ -27,16 +27,16 @@ namespace {// to avoid poluting
 
   struct InitArgs
   {
-    const ACorrelation::OutputData &out;
-    const ACorrelation::InputData &in;
+    const AFP::OutputData &out;
+    const AFP::InputData &in;
     const Settings &settings;
-    std::vector<std::unique_ptr<ACorrelation>> &algs;
+    std::vector<std::unique_ptr<AFP>> &algs;
   };
 
   void CpuInit(void *args)
   {
     auto *a = reinterpret_cast<InitArgs *>(args);
-    auto alg = std::make_unique<Correlation_CPU>();
+    auto alg = std::make_unique<FP_CPU>();
     if (alg->Init(a->out, a->in, a->settings)) {
       a->algs[static_cast<size_t>(starpu_worker_get_id())] = std::move(alg);
     }
@@ -46,23 +46,21 @@ namespace {// to avoid poluting
   {
     auto *a = reinterpret_cast<InitArgs *>(args);
     std::vector<CUstream> stream = { starpu_cuda_get_local_stream() };
-    auto alg = std::make_unique<Correlation_CUDA>(starpu_worker_get_id(), stream);
+    auto alg = std::make_unique<FP_CUDA>(starpu_worker_get_id(), stream);
     if (alg->Init(a->out, a->in, a->settings)) {
       a->algs[static_cast<size_t>(starpu_worker_get_id())] = std::move(alg);
     }
   }
 }// namespace
 
-bool CorrelationStarPU::Init(const StarpuOutputData &out,
-  const StarpuInputData &in,
-  const Settings &s)
+bool FPStarPU::Init(const StarpuOutputData &out, const StarpuInputData &in, const Settings &s)
 {
-  return ACorrelation::Init(out.GetCorrelations()->GetPayload(),
-    { in.GetData1()->GetPayload(), in.GetData2()->GetPayload() },
+  return AFP::Init(out.GetData()->GetPayload(),
+    { in.GetData()->GetPayload(), in.GetFilter()->GetPayload() },
     s);
 }
 
-bool CorrelationStarPU::InitImpl()
+bool FPStarPU::InitImpl()
 {
   const auto &out = this->GetOutputRef();
   const auto &in = this->GetInputRef();
@@ -79,18 +77,19 @@ bool CorrelationStarPU::InitImpl()
   return (algs.size()) > 0;
 }
 
-bool CorrelationStarPU::ExecuteImpl(const OutputData &out, const InputData &in)
+bool FPStarPU::ExecuteImpl(const OutputData &out, const InputData &in)
 {
   bool res = false;
   if ((nullptr == outPtr) && (nullptr == inPtr)) {
     // input and output data are not Starpu Payloads
-    using Type = data::StarpuPayload<InputData::PayloadType::LDType>;
-    auto o = StarpuOutputData(std::make_unique<Type>(out.GetCorrelations()));
-    auto i =
-      StarpuInputData(std::make_unique<Type>(in.GetData1()), std::make_unique<Type>(in.GetData2()));
+    using DType = data::StarpuPayload<InputData::DataType::LDType>;
+    using FType = data::StarpuPayload<InputData::FilterType::LDType>;
+    auto o = StarpuOutputData(std::make_unique<DType>(out.GetData()));
+    auto i = StarpuInputData(
+      std::make_unique<DType>(in.GetData()), std::make_unique<FType>(in.GetFilter()));
     res = ExecuteImpl(o, i);
     // assume that results are requested
-    o.GetCorrelations()->Unregister();
+    o.GetData()->Unregister();
   } else {
     // input and output are Starpu Payload, stored locally
     res = ExecuteImpl(*outPtr, *inPtr);
@@ -98,26 +97,26 @@ bool CorrelationStarPU::ExecuteImpl(const OutputData &out, const InputData &in)
   return res;
 }
 
-bool CorrelationStarPU::Execute(const StarpuOutputData &out, const StarpuInputData &in)
+bool FPStarPU::Execute(const StarpuOutputData &out, const StarpuInputData &in)
 {
   // store the reference to payloads locally
   outPtr = &out;
   inPtr = &in;
   // call normal execute with the normal Payloads to get all checks etc.
-  auto res = ACorrelation::Execute(out.GetCorrelations()->GetPayload(),
-    { in.GetData1()->GetPayload(), in.GetData2()->GetPayload() });
+  auto res = AFP::Execute(
+    out.GetData()->GetPayload(), { in.GetData()->GetPayload(), in.GetFilter()->GetPayload() });
   // cleanup
   outPtr = nullptr;
   inPtr = nullptr;
   return res;
 }
 
-bool CorrelationStarPU::ExecuteImpl(const StarpuOutputData &out, const StarpuInputData &in)
+bool FPStarPU::ExecuteImpl(const StarpuOutputData &out, const StarpuInputData &in)
 {
   struct starpu_task *task = starpu_task_create();
-  task->handles[0] = out.GetCorrelations()->GetHandle();
-  task->handles[1] = in.GetData1()->GetHandle();
-  task->handles[2] = in.GetData2()->GetHandle();
+  task->handles[0] = out.GetData()->GetHandle();
+  task->handles[1] = in.GetData()->GetHandle();
+  task->handles[2] = in.GetFilter()->GetHandle();
   task->workerids = CreateWorkerMask(task->workerids_len,
     algs);// FIXME bug in the StarPU? If the mask is completely 0, codelet is being invoked anyway
   task->cl_arg = new ExecuteArgs{ this->GetSettings(), &algs };
@@ -138,4 +137,4 @@ bool CorrelationStarPU::ExecuteImpl(const StarpuOutputData &out, const StarpuInp
   STARPU_CHECK_RETURN_VALUE(starpu_task_submit(task), "starpu_task_submit %s", this->taskName);
   return true;
 }
-}// namespace umpalumpa::correlation
+}// namespace umpalumpa::fourier_processing
