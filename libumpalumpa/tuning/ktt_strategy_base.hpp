15abff3461602dd0a919b491eb936cc42a64b59c
David Myska <456346@mail.muni.cz>
2021-11-02 18:22:14 +0100

    Remove unnecessary 'using'

--- libumpalumpa/tuning/ktt_strategy_base.hpp
+++ libumpalumpa/tuning/ktt_strategy_base.hpp
@@ -12,16 +12,12 @@ class KTTStrategyBase
   , public algorithm::TunableStrategy
 {
 public:
-  // NOTE not sure why, but writing it this way is the only way how it works.
-  // It has to have the 'Strategy =' part aswell as the 'typename', otherwise it breaks the
-  // successor classes.
-  using Strategy = typename BasicAlgorithm<O, I, S>::Strategy;
-
   // FIXME catch std::bad_cast at a reasonable place and react accordingly
   // Hypothetically, dynamic_cast should always succeed, because all the algorithms that use KTT
   // have to inherit from KTT_Base, and only such algorithms are passed into this constructor
   KTTStrategyBase(const BasicAlgorithm<O, I, S> &algorithm)
-    : Strategy(algorithm), TunableStrategy(dynamic_cast<const KTT_Base &>(algorithm).GetHelper())
+    : BasicAlgorithm<O, I, S>::Strategy(algorithm),
+      TunableStrategy(dynamic_cast<const KTT_Base &>(algorithm).GetHelper())
   {}
 
   virtual bool InitImpl() = 0;
