0794b1fc0847ced9ec00a9466da270beab230310
David Strelak <davidstrelak@gmail.com>
2022-01-04 14:21:02 +0100

    adding new util method for bool expansion to template

--- libumpalumpa/algorithms/fourier_processing/fp_cpu_kernels.hpp
+++ libumpalumpa/algorithms/fourier_processing/fp_cpu_kernels.hpp
@@ -4,32 +4,34 @@
 
 namespace umpalumpa::fourier_processing {
 
-template<typename T, typename T2>
-void scaleFFT2DCPU(const T2 *__restrict__ in,
-  T2 *__restrict__ out,
-  const umpalumpa::data::Size &inSize,
-  const umpalumpa::data::Size &outSize,
-  const T *__restrict__ filter,
-  float normFactor,
-  bool applyFilter,
-  bool normalize,
-  bool center)
+template<bool applyFilter, bool normalize, bool center>
+struct ScaleFFT2DCPU
 {
-  for (size_t n = 0; n < inSize.n; ++n) {
-    for (size_t y = 0; y < outSize.y; ++y) {
-      for (size_t x = 0; x < outSize.x; ++x) {
-        size_t origY = (y <= outSize.y / 2) ? y : (inSize.y - (outSize.y - y));
-        size_t iIndex = n * inSize.single + origY * inSize.x + x;
-        size_t oIndex = n * outSize.single + y * outSize.x + x;
-        out[oIndex] = in[iIndex];
-        if (applyFilter) { out[oIndex] *= filter[y * outSize.x + x]; }
-        if (normalize) { out[oIndex] *= normFactor; }
-        if (center) {
-          out[oIndex] *= static_cast<T>(1 - 2 * ((static_cast<int>(x) + static_cast<int>(y)) & 1));
+  template<typename T, typename T2>
+  static void Execute(const T2 *__restrict__ in,
+    T2 *__restrict__ out,
+    const umpalumpa::data::Size &inSize,
+    const umpalumpa::data::Size &outSize,
+    const T *__restrict__ filter,
+    float normFactor)
+  {
+    for (size_t n = 0; n < inSize.n; ++n) {
+      for (size_t y = 0; y < outSize.y; ++y) {
+        for (size_t x = 0; x < outSize.x; ++x) {
+          size_t origY = (y <= outSize.y / 2) ? y : (inSize.y - (outSize.y - y));
+          size_t iIndex = n * inSize.single + origY * inSize.x + x;
+          size_t oIndex = n * outSize.single + y * outSize.x + x;
+          out[oIndex] = in[iIndex];
+          if (applyFilter) { out[oIndex] *= filter[y * outSize.x + x]; }
+          if (normalize) { out[oIndex] *= normFactor; }
+          if (center) {
+            out[oIndex] *=
+              static_cast<T>(1 - 2 * ((static_cast<int>(x) + static_cast<int>(y)) & 1));
+          }
         }
       }
     }
   }
-}
+};
 
 }// namespace umpalumpa::fourier_processing
