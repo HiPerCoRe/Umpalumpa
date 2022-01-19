f17aed39380d9afacccfe7df990762a3d7645462
David Myska <456346@mail.muni.cz>
2022-01-19 16:20:02 +0100

    Added single strategy, single kernel tests

--- tests/tuning/tuning/general_tuning_tests.cpp
+++ tests/tuning/tuning/general_tuning_tests.cpp
@@ -1,12 +1,16 @@
 #include <gtest/gtest.h>
 #include <tests/tuning/waiting_algorithm.hpp>
+#include <iostream>
 
 // tests:
-// strategy never tuned -> run default config
-// strategy is tuning -> run various configs
-// strategy already tuned -> run best found config
-// strategy tuned -> run best -> continue tuning -> run best
-// multiple strategies...
+// DONE strategy never tuned -> run default config
+// DONE strategy is tuning -> run various configs
+// DONE strategy already tuned -> run best found config
+// DONE strategy tuned -> run best -> continue tuning -> run best
+// multiple same strategies (should not interfere if not equal, should reuse tuned stuff when
+// equal/similar)
+// multiple different strategies (should not interfere, can tune just specified
+// subset)
 
 using namespace umpalumpa;
 using namespace umpalumpa::data;
@@ -28,8 +32,111 @@ protected:
   WaitingAlgorithm::InputData out;
 
   WaitingAlgorithm alg = WaitingAlgorithm(0);
+
+  constexpr static auto manualCheckMsg = "\n\t## Manual check -- ";
 };
 
+TEST_F(GeneralTuningTests, no_tuning_should_run_default_config)
+{
+  settings = { 1, 1 };
+
+  ASSERT_TRUE(alg.Init(out, in, settings));
+  std::cout << manualCheckMsg << "Expecting default config (1 time):" << std::endl;
+  ASSERT_TRUE(alg.Execute(out, in));
+}
+
+TEST_F(GeneralTuningTests, no_tuning_should_run_default_config_multiple_times)
+{
+  settings = { 2, 2 };
+
+  ASSERT_TRUE(alg.Init(out, in, settings));
+  std::cout << manualCheckMsg << "Expecting default configs (3 times):" << std::endl;
+  ASSERT_TRUE(alg.Execute(out, in));
+  ASSERT_TRUE(alg.Execute(out, in));
+  ASSERT_TRUE(alg.Execute(out, in));
+}
+
+TEST_F(GeneralTuningTests, tuning_is_on_various_configs_should_be_tried)
+{
+  settings = { 3, 3 };
+
+  ASSERT_TRUE(alg.Init(out, in, settings));
+  // After successful Init there is a strategy to work with!
+
+  auto &strat = dynamic_cast<TunableStrategy &>(alg.GetStrategy());
+  strat.SetTuningApproach(TuningApproach::kEntireStrategy);
+
+  std::cout << manualCheckMsg << "Expecting various configs (3 times):" << std::endl;
+  ASSERT_TRUE(alg.Execute(out, in));
+  ASSERT_TRUE(alg.Execute(out, in));
+  ASSERT_TRUE(alg.Execute(out, in));
+}
+
+TEST_F(GeneralTuningTests, tune_strategy_then_run_best_found_config)
+{
+  settings = { 4, 4 };
+
+  ASSERT_TRUE(alg.Init(out, in, settings));
+  // After successful Init there is a strategy to work with!
+
+  auto &strat = dynamic_cast<TunableStrategy &>(alg.GetStrategy());
+  strat.SetTuningApproach(TuningApproach::kEntireStrategy);
+
+  // Tuning executes
+  std::cout << manualCheckMsg << "Expecting various configs (2 times):" << std::endl;
+  ASSERT_TRUE(alg.Execute(out, in));
+  ASSERT_TRUE(alg.Execute(out, in));
+
+  // Turn off tuning
+  strat.SetTuningApproach(TuningApproach::kNoTuning);
+
+  // Best config executes
+  std::cout << manualCheckMsg << "Expecting the best found configs (2 times):" << std::endl;
+  ASSERT_TRUE(alg.Execute(out, in));
+  ASSERT_TRUE(alg.Execute(out, in));
+}
+
+TEST_F(GeneralTuningTests, tune_then_run_best_tune_again_then_run_best)
+{
+  settings = { 5, 5 };
+
+  ASSERT_TRUE(alg.Init(out, in, settings));
+  // After successful Init there is a strategy to work with!
+
+  auto &strat = dynamic_cast<TunableStrategy &>(alg.GetStrategy());
+  strat.SetTuningApproach(TuningApproach::kEntireStrategy);
+
+  // First tuning executes
+  std::cout << manualCheckMsg << "Expecting various configs (2 times):" << std::endl;
+  ASSERT_TRUE(alg.Execute(out, in));
+  ASSERT_TRUE(alg.Execute(out, in));
+
+  // Turn off tuning
+  strat.SetTuningApproach(TuningApproach::kNoTuning);
+
+  // Best config (of first tuning) executes
+  std::cout << manualCheckMsg << "Expecting the best found configs (2 times):" << std::endl;
+  ASSERT_TRUE(alg.Execute(out, in));
+  ASSERT_TRUE(alg.Execute(out, in));
+
+  // Turn on tuning
+  strat.SetTuningApproach(TuningApproach::kEntireStrategy);
+
+  // Second tuning executes
+  std::cout << manualCheckMsg << "Expecting various configs (2 times):" << std::endl;
+  ASSERT_TRUE(alg.Execute(out, in));
+  ASSERT_TRUE(alg.Execute(out, in));
+
+  // Turn off tuning
+  strat.SetTuningApproach(TuningApproach::kNoTuning);
+
+  // Best config (of all tunings) executes
+  std::cout << manualCheckMsg << "Expecting the best found configs (2 times):" << std::endl;
+  ASSERT_TRUE(alg.Execute(out, in));
+  ASSERT_TRUE(alg.Execute(out, in));
+}
+
+/*
 TEST_F(GeneralTuningTests, TODOname)
 {
   // Set Settings
@@ -45,3 +152,5 @@ TEST_F(GeneralTuningTests, TODOname)
 
   // TODO checks
 }
+*/
+
--- tests/tuning/waiting_kernel.cu
+++ tests/tuning/waiting_kernel.cu
@@ -7,7 +7,7 @@
 __global__ void waitingKernel() { __nanosleep(1000000u * MILLISECONDS); }
 #else
 // This version uses just very rough estimation of milliseconds
-// We don't query the GPU's frequency dynamically, we chose the number 2 GHz
+// We don't query the GPU's frequency dynamically, we chose the number 1.5 GHz
 
 // Inspired by:
 // https://stackoverflow.com/questions/11217117/equivalent-of-usleep-in-cuda-kernel
@@ -20,7 +20,7 @@ __global__ void waitingKernel()
   for (;;) {
     now = clock64();
     long long cycles = now - start;// ignore overflow... should not happen
-    long long elapsedMiliseconds = (cycles / 2000000000.0f) * 1000.0f;
+    long long elapsedMiliseconds = (cycles / 1500000000.0f) * 1000.0f;
     if (elapsedMiliseconds >= MILLISECONDS) { break; }
   }
   // Makes sure that compiler doesn't optimize the loop away
