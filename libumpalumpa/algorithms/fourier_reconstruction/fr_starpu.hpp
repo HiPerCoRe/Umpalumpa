20f227835de273b81ab1937cc03458765007d715
David Strelak <davidstrelak@gmail.com>
2022-01-26 18:43:23 +0100

    adding starpu version

---
+++ libumpalumpa/algorithms/fourier_reconstruction/fr_starpu.hpp
@@ -0,0 +1,30 @@
+#pragma once
+#include <libumpalumpa/algorithms/fourier_reconstruction/afr.hpp>
+#include <queue>
+
+// forward declaration
+struct starpu_task;
+
+namespace umpalumpa::fourier_reconstruction {
+class FRStarPU final : public AFR
+{
+public:
+  ~FRStarPU();
+
+  void Cleanup() override;
+
+  void Synchronize() override;
+
+protected:
+  bool InitImpl() override;
+  bool ExecuteImpl(const OutputData &out, const InputData &in);
+
+private:
+  inline static const std::string taskName = "FourierReconstructionStarPU";
+
+  std::vector<AFR *> algs;
+  long noOfInitWorkers = 0;
+
+  std::queue<starpu_task *> taskQueue;
+};
+}// namespace umpalumpa::fourier_reconstruction
